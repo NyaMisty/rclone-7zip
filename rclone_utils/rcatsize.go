2d760f4e554670f6145f2050f2a53b0980e5dee0
Misty <gyc990326@gmail.com>
2022-07-29 19:17:23 +0800

    rcatsize: adjust async layout to achieve better performance

--- rclone_utils/rcatsize.go
+++ rclone_utils/rcatsize.go
@@ -14,53 +14,75 @@ import (
 
 func (r *RcloneUtil) RcatSize(path string, size int64, bufferSize int64, asyncCallback func(resp interface{}, err error)) (writer io.WriteCloser, err error) {
 	if r.RcloneMode == "rc" {
-		_fifoTmpFile, err := ioutil.TempFile("", "rcatsize")
-		if err != nil {
-			return nil, err
-		}
-		fifoTmpPath := _fifoTmpFile.Name()
-		_fifoTmpFile.Close()
-		os.Remove(fifoTmpPath)
-		err = unix.Mkfifo(fifoTmpPath, 0o666)
-		if err != nil {
-			log.Errorf("Failed to create fifo %s (%v)", _fifoTmpFile, err)
-			return nil, err
-		}
-		log.Debugf("Created fifo %s (%v)", fifoTmpPath, err)
 		reader, writer := io.Pipe()
 
-		pathSplitTmp := strings.SplitN(path, ":", 2)
-		dstFs := "/"
-		dstRemote := path
-		if len(pathSplitTmp) == 2 {
-			dstFs = pathSplitTmp[0] + ":"
-			dstRemote = pathSplitTmp[1]
-		}
-		log.Debugf("rcatSize dstFs: %v dstRemote: %v", dstFs, dstRemote)
-
-		cleanup := func() {
-			writer.Close()
-			reader.Close()
-			os.Remove(fifoTmpPath)
-		}
-
 		go func() {
-			_, err = r._doRcReq("operations/rcatsize", map[string]interface{}{
-				"type":   "fifo",
-				"addr":   fifoTmpPath,
-				"size":   size,
-				"fs":     dstFs,
-				"remote": dstRemote,
-			}, asyncCallback)
+			//r.maxTransferSem.Acquire(context.Background(), 1)
+			//defer r.maxTransferSem.Release(1)
 
-			r.maxTransferSem.Acquire(context.Background(), 1)
-			defer r.maxTransferSem.Release(1)
+			// Create FIFO
+			_fifoTmpFile, err := ioutil.TempFile("", "rcatsize")
+			if err != nil {
+				asyncCallback(nil, err)
+				return
+			}
+			fifoTmpPath := _fifoTmpFile.Name()
+			_fifoTmpFile.Close()
+			os.Remove(fifoTmpPath)
+			err = unix.Mkfifo(fifoTmpPath, 0o666)
+			if err != nil {
+				log.Errorf("Failed to create fifo %s (%v)", _fifoTmpFile, err)
+				asyncCallback(nil, err)
+				return
+			}
+			log.Debugf("Created fifo %s (%v)", fifoTmpPath, err)
+
+			cleanup := func() {
+				log.Debugf("Cleanup for %s, fifo %s!", path, fifoTmpPath)
+				writer.Close()
+				reader.Close()
+				err := os.Remove(fifoTmpPath)
+				if err != nil {
+					log.Infof("fifoRemove %s, err %v", fifoTmpPath, err)
+				}
+			}
+			go func() {
+				pathSplitTmp := strings.SplitN(path, ":", 2)
+				dstFs := "/"
+				dstRemote := path
+				if len(pathSplitTmp) == 2 {
+					dstFs = pathSplitTmp[0] + ":"
+					dstRemote = pathSplitTmp[1]
+				}
+				log.Debugf("rcatSize dstFs: %v dstRemote: %v", dstFs, dstRemote)
+
+				r.maxTransferSem.Acquire(context.Background(), 1)
+				asyncCallbackWrap := func(resp interface{}, err error) {
+					defer r.maxTransferSem.Release(1)
+					asyncCallback(resp, err)
+				}
+
+				_, err = r._doRcReq("operations/rcatsize", map[string]interface{}{
+					"type":   "fifo",
+					"addr":   fifoTmpPath,
+					"size":   size,
+					"fs":     dstFs,
+					"remote": dstRemote,
+				}, asyncCallbackWrap)
+				if err != nil {
+					cleanup()
+					asyncCallbackWrap(nil, err)
+					return
+				}
+			}()
 
 			// this open call will block until rclone connects
 			fifoWriter, err := os.OpenFile(fifoTmpPath, os.O_WRONLY, os.ModeNamedPipe)
 			if err != nil {
 				//return nil, err
 				log.Errorf("Failed to open fifo file, err: %v", err)
+				cleanup()
+				return
 			}
 			log.Debugf("Rclone opened the fifo, start transmitting!")
 
@@ -76,12 +98,8 @@ func (r *RcloneUtil) RcatSize(path string, size int64, bufferSize int64, asyncCa
 				cleanup()
 			})
 			log.Debugf("RcatSize ioCopy reader finished n: %d, err: %v", n, err)
+			// we can't cleanup here as BetterCopy's writer may still pending
 		}()
-		if err != nil {
-			asyncCallback(nil, err)
-			cleanup()
-			return nil, err
-		}
 
 		return writer, nil
 	} else {
